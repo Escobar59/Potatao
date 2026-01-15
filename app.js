d7893333c99b334fdcede97287c75a27a2c1ff65
Clement Cotar <clementcotar59@gmail.com>
2026-01-15 10:58:10 +0100

    ajout de la V1 des pages articles connexion et acceuil
    ajout des pages

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
-
+app.set('view engine', 'ejs');      // 👈 TRÈS IMPORTANT
+app.set('views', './views');    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send("Mini site Web avec Express !");
+app.get('/', (req, res) => {
+    res.render('index');
 });
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/articles', (req, res) => {
+    res.render('articles');
+});
+
+
+
 
 app.listen(3000, () => {
   console.log("Serveur lancé sur http://localhost:3000");
